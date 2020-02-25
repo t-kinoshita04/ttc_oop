adbb8f7fc8c1c7bdbea66152e8ffd160ee3fc055
TTCAD\t-kinoshita <t-kinoshita@ttc-net.co.jp>
2020-02-25 13:46:44 +0900

    特別講義_20200225

---
+++ work/go/wiki3.go
@@ -0,0 +1,27 @@
+﻿package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+    "time"
+)
+
+func handlerA(w http.ResponseWriter, r *http.Request) {
+	for distance := 50; distance <= 150; distance++ {
+		fmt.Fprintf(w, "%s: %d\n", r.URL.Path[1:], distance)
+	}
+}
+
+func handlerB(w http.ResponseWriter, r *http.Request) {
+	for distance := 0.0; distance <= 150.0; distance += 1.5 {
+		fmt.Fprintf(w, "%s: %.1f\n", r.URL.Path[1:], distance)
+    }
+    time.Sleep(5000 * time.Millisecond)
+}
+
+func main() {
+	http.HandleFunc("/a_san", handlerA)
+	http.HandleFunc("/b_san", handlerB)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
