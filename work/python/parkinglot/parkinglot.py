d87e8f3e6d2a43fe4535a01120ba6563841c9598
TTCAD\t-kinoshita <t-kinoshita@ttc-net.co.jp>
2020-03-03 13:12:37 +0900

    特別講義_20200303

---
+++ work/python/parkinglot/parkinglot.py
@@ -0,0 +1,15 @@
+﻿class ParkingSpot:
+    def __init__(self, size):
+        self.size = size
+        self.vechile = None            
+
+    def getVehicle(self):
+        return self.vechile
+
+    def parkVehicle(self, vechile):
+        self.vechile = vechile
+
+
+class Vehicle:
+
+    def canFitInSpot():
---
+++ work/python/parkinglot/test_parkinglot.py
@@ -0,0 +1,13 @@
+﻿import unittest
+import parkinglot as pkl
+
+class TestParkingSpot(unittest.TestCase):
+    def test_parkingVehicle(self):
+        parkingSpot = pkl.ParkingSpot(pkl.Vehicle.Size.MOTOR_CYCLE)
+        car = pkl.Car(pkl.VehicleSize.MOTOR_CYCLE)
+        parkingSpot.parkVehicle(car)
+
+        self.assertEqual(car, parkingSpot.getVehicle())
+
+if __name__ == '__main__':
+    unittest.main()
