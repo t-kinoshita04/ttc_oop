586a77e677e6dc542428ae9a31ed63e332b92242
TTCAD\t-kinoshita <t-kinoshita@ttc-net.co.jp>
2020-03-10 11:36:38 +0900

    特別講義_20200310

---
+++ work/python/test/user_type.py
@@ -0,0 +1,7 @@
+﻿def button_available(employee):
+    if employee == "社員":
+        return True
+    elif employee == "契約社員":
+        return True
+    elif employee == "派遣社員":
+        return False
